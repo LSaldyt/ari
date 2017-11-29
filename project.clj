1de7c77cd6c383125bab05cba4dbcebf3038ec9c
LSaldyt <lucassaldyt@gmail.com>
2017-11-29 12:56:08 -0700

    Adds json core input/output

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.4"]]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-bin "0.3.4"]]
   :bin { :name "app" }
   :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,10 +1,19 @@
 (ns app.core
-  (:require [clojure.tools.cli :refer [cli]])
+  (:require [clojure.tools.cli :refer [cli]]
+            [clojure.data.json :as json])
   (:gen-class))
+
+(defn transform [from]
+  {"test" "a"})
+
+(defn process-json []
+  (let [input (json/read-str (slurp "in.json"))]
+  (spit "out.json" (json/write-str (transform input)))))
 
 (defn -main [& args]
   (let [[opts args banner] (cli args
     ["-h" "--help" "Print this help"
      :default false :flag true])]
     (when (:help opts)
-      (println banner))))
+      (println banner)))
+  (process-json))
