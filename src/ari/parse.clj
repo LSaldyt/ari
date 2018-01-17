0a5d68b67eb71b77bd4b3884583bb2d1dfcbcba5
LSaldyt <lucassaldyt@gmail.com>
2018-01-17 13:21:58 -0700

    Improves parser results

--- src/ari/parse.clj
+++ src/ari/parse.clj
@@ -9,7 +9,13 @@
 ; If there are no parsers left, throw an error.
 ; Once there is no input left, create a parse tree with the longest parser. If two syntax elements are ambiguous, throw an error.
 
+; Parser structure
+; [tokens] -> {tree}, [remaining tokens]
+
+; assignment([[x ident] [= op] [2 int]]) -> {:assignment {:name x :op = :value 2}}
+
 ; Basic parser elements:
+; 
 ; inOrder 
 ; just
 ; optional
@@ -22,34 +28,51 @@
 (def any "*any*")
 
 (defn just 
-  ([etoken]
-    (fn [[token tag]] 
-      (or (= token etoken) (= etoken any))))
+  "((just any 'int') [2 'int']) -> [token tag]"
   ([etoken etag]
+   (just etoken etag nil))
+  ([etoken etag k]
     (fn [[token tag]]
-      (and 
-        (or (= etoken token) (= etoken any))
-        (or (= etag tag)     (= etag any))))))
+      (if (and 
+            (or (= etoken token) (= etoken any))
+            (or (= etag tag)     (= etag any)))
+        [token tag k]
+        nil))))
+
+(defn tag
+  ([etag]
+    (just any etag))
+  ([etag k]
+    (just any etag k)))
+
+(defn token
+  ([etoken]
+    (just etoken any))
+  ([etoken k]
+    (just etoken any k)))
+
+(defn wild
+  ([] (just any any))
+  ([k] (just any any k)))
+
 
 (defn inorder [given-parsers]
   (fn [tokens] (loop [parsers   given-parsers
-                      remaining tokens]
+                      remaining tokens
+                      consumed  {}]
                  (if (or (empty? remaining) (empty? parsers))
-                   true
-                   (and ((first parsers) (first remaining))
-                     (recur (rest parsers)
-                             (rest remaining)))))))
+                   (if (empty? consumed) nil [consumed remaining])
+                   (let [result ((first parsers) (first remaining))]
+                     (if result 
+                       (recur (rest parsers)
+                              (rest remaining)
+                              (let [[token tag k] result] 
+                                (if (nil? k)
+                                  consumed
+                                  (assoc consumed k [token tag]))))
+                       (if (empty? consumed) nil [consumed remaining])))))))
 
-(def test-assignment (inorder [(just any) (just " ") (just "=") (just " ") (just any "int") (just any)]))
-
-;; (println (first [[" " "whitespace"]]))
-;; (println ((inorder [(just any) (just " ")]) [["a" "test"] [" " "whitespace"]]))
-;; (println (test-assignment [["a" "ident"] [" " "whitespace"] ["=" "operator"] [" " "whitespace"] ["2" "int"]]))
-;; (println ((just any) ["a" "ident"]))
-;; (println ((just " ") [" " "whitespace"]))
-;; (println ((just "=") ["=" "operator"]))
-;; (println ((just " ") [" " "whitespace"]))
-;; (println ((just any "int") ["2" "int"]))
+(def test-assignment (inorder [(wild :name) (token " ") (token "=" :op) (token " ") (tag "int" :value) (wild)]))
 
 (defn parse [content]
   (println content)
