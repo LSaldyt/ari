5119768db642efa13048105d6ce7a67a59bc49d2
LSaldyt <lucassaldyt@gmail.com>
2018-02-06 20:24:47 -0700

    Adds sepby test

--- test/ari/core_test.clj
+++ test/ari/core_test.clj
@@ -1,7 +1,22 @@
 (ns ari.core_test
   (:require [clojure.test :refer :all]
-            [ari.core :refer :all]))
+            [ari.core :refer :all]
+            [ari.parse :refer :all]))
 
-(deftest a-test
-  (testing "Assertion of basic numerical truth."
-    (is (= 0 0))))
+(def test-tokens [["a" "name"] ["|" "pipe"] ["b" "name"]])
+
+(deftest sep-test
+  (testing "sepby"
+    (is (= 2 (count (:values (first
+      ((sep-by (tag "name" :name) (token "|"))
+               test-tokens
+               {}))))))
+    (is (nil? (first 
+      ((sep-by1 (tag "name" :name) (token "|"))
+       [["a" "name"]]
+       {}))))
+    (is (= 2 (count (:values (first
+      ((sep-by1 (tag "name" :name) (token "|"))
+               test-tokens
+               {}
+               ))))
